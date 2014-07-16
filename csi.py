0ae8d259bf035a4b50168cd6289cce7023e3c092
Bruno Siqueira <bsiqueira@gmail.com>
2014-07-16 17:42:49 -0300

    Update csi.py

--- csi.py
+++ csi.py
@@ -4,8 +4,8 @@ import urllib, urllib2
 import json
 
 
-def outputCsv(data):
-    with open('output.csv', 'w') as fp:
+def outputCsv(fileName, data):
+    with open(fileName, 'w') as fp:
         a = csv.writer(fp, delimiter=',')
         a.writerows(data)
 
@@ -16,39 +16,65 @@ try:
     rows = list(reader)
     print len(rows)
     count = 0
-    for row in rows:
+    rows[0].insert(47, "latIdentification")
+    rows[0].insert(48, "longIdentification")
+    outputRows.append(rows[0])
+    for x in range(1, len(rows)):
+        row = rows[x]
         latitude = row[2]
         longitude = row[3]
         location = row[6]
         identification = row[8]
+        age = row[9]
+        randomQuestion = row[25]
         searchStr = 'null'
-        if(location != 'null' and identification != 'null'):
-            searchStr = location + ', ' + identification
-        elif(location != 'null'):
-            searchStr = location
-        else:
-            searchStr = identification
-        if((latitude == 'null' or longitude == 'null') and searchStr != 'null'):
-            count=count+1
-            print 'params=[',params,']'
-            params = {'address': searchStr + ', '+sys.argv[2], 'sensor': 'false'} 
-            url = 'http://maps.googleapis.com/maps/api/geocode/json?' + urllib.urlencode(params)
+        if (age != 'null' or randomQuestion != 'null' or ((latitude!= 'null' and longitude!='null') or (location != 'null') )):
+            # if(location != 'null' and identification != 'null'):
+            #     searchStr = location + ', ' + identification
+            # elif(location != 'null'):
+            #     searchStr = location
+            # else:
+            #     searchStr = identification
+            if(latitude == 'null' or longitude == 'null'):
+                count=count+1
+                searchStr = location
+                print 'searching for str=[',searchStr,']'
+                params = {'address': searchStr + ', '+sys.argv[2], 'sensor': 'false'} 
+                url = 'http://maps.googleapis.com/maps/api/geocode/json?' + urllib.urlencode(params)
 
-            rawreply = urllib2.urlopen(url).read()                                          
-            reply = json.loads(rawreply)                                                    
+                rawreply = urllib2.urlopen(url).read()                                          
+                reply = json.loads(rawreply)                                                    
             
-            lat = reply['results'][0]['geometry']['location']['lat']                        
-            lng = reply['results'][0]['geometry']['location']['lng']                        
+                lat = reply['results'][0]['geometry']['location']['lat']                        
+                lng = reply['results'][0]['geometry']['location']['lng']                        
             
-            print '[%f; %f]' % (lat, lng)
-            row[2] = lat
-            row[3] = lng
+                print '[%f; %f]' % (lat, lng)
+                row[2] = lat
+                row[3] = lng
+            if(identification!='null' ):
+                searchStr = identification
+                print 'searching for str=[',searchStr,']'
+                params = {'address': searchStr + ', '+sys.argv[2], 'sensor': 'false'} 
+                url = 'http://maps.googleapis.com/maps/api/geocode/json?' + urllib.urlencode(params)
+
+                rawreply = urllib2.urlopen(url).read()                                          
+                reply = json.loads(rawreply)                                                    
+            
+                lat = reply['results'][0]['geometry']['location']['lat']                        
+                lng = reply['results'][0]['geometry']['location']['lng']                        
+            
+                print '[%f; %f]' % (lat, lng)
+                row.insert(47, lat)
+                row.insert(48, lng)
+            else:
+                row.insert(47, 'null')
+                row.insert(48, 'null')
             outputRows.append(row)
-        else:
-            outputRows.append(row)
+            
+        #print latitude
     print 'without latLng=['+str(count)+']'
 finally:
     print 'outputing to csv...'
-    outputCsv(outputRows)
+    outputCsv("out_"+sys.argv[1], outputRows)
     print 'done...'
     f.close()
